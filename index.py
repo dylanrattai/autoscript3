64f053a2b7ccd26244d798503e938acc17c54cfb
dylanrattai <817682@mystma.org>
2022-10-26 14:47:44 -0500

    working on gui stuff

--- index.py
+++ index.py
@@ -1,16 +1,46 @@
+from re import sub
 from tkinter import *
 
 root = Tk()
 
+def save():
+    return "saved"
+
+def next():
+    return "next"
+
+def last():
+    return "last"
+
+def delete():
+    return "deleted"
+
+def copy():
+    return "copied"
+
+def print():
+    return "printing"
+
 blank = Label(root, text = "   ")
 mainTitle = Label(root, text = "Autoscript 3\n", font=("Impact, 32"))
-viewlast = Label(root, text = "Back")
-viewNext = Label(root, text = "Next")
-saveButton = Label(root, text = "Save")
-deleteButton = Label(root, text = "Delete")
-copyButton = Label(root, text = "Copy")
-printButton = Label(root, text = "Print")
-
-mainTitle.grid(row=1, column=0)
+viewlast = Button(root, text = "Back", command = last())
+viewNext = Button(root, text = "Next", command = next())
+saveButton = Button(root, text = "Save", command = save())
+deleteButton = Button(root, text = "Delete", command = delete())
+copyButton = Button(root, text = "Copy", command = copy())
+printButton = Button(root, text = "Print", command = print())
+announcementTitle = Label(root, text = "Add variable here")
+position = Label(root, text = "[" + "Add variable here" + "/" + "Add variable here" + "]")
+submitter = Label(root, text = "Add variable here")
+submitterFirst = Label(root, text = "(" + "Add variable here" + ")")
+submissionDate = Label(root, text = "Add variable here")
+startAir = Label(root, text = "Add variable here")
+endAir = Label(root, text = "Add variable here")
+displayDays = Label(root, text = "Add variable here")
+submissionDateText = Label(root, text = "Submission Date:", font='Helvetica 18 bold')
+startAirText = Label(root, text = "Add variable here", font='Helvetica 18 bold')
+endAirText = Label(root, text = "Add variable here", font='Helvetica 18 bold')
+displayDaysText = Label(root, text = "Add variable here", font='Helvetica 18 bold')
+submitterText = Label(root, "Submitted By", font='Helvetica 18 bold')
 
 root.mainloop()
--- main.py
+++ main.py
@@ -6,18 +6,19 @@ def done():
     return done
 
 blank = Label(root, text = "   ")
-mainTitle = Label(root, text = "Autoscript 3\n", font=("Impact, 32"))
+mainTitle = Label(root, text = "Autoscript 3\n", font=("Helvetica 32 bold"))
 anchor1 = Label(root, text = "Anchor 1:")
 anchor2 = Label(root, text = "Anchor 2:")
-doneButton = Button(root, text = "Done", command = done)
+doneButton = Button(root, text = "Done", command = done())
 sheetLink = Label(root, text = "Google Sheets File")
 inputAnchor1 = Entry(root)
 inputAnchor2 = Entry(root)
 inputSheetLink = Entry(root)
 
+#CHANGE TO FRAMES \/
 mainTitle.grid(row=1, column=0)
-sheetLink.grid(row=2, column=0)
-inputSheetLink.grid(row=2, column=1)
+#sheetLink.grid(row=2, column=0)
+#inputSheetLink.grid(row=2, column=1)
 anchor1.grid(row=4, column=0)
 inputAnchor1.grid(row=4, column=1)
 anchor2.grid(row=5, column=0)
