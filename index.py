3ed542a12fcd0853d849fae1f9a0800c4f5ae758
dylanrattai <817682@mystma.org>
2022-10-28 15:19:55 -0500

    lots of frame

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+#main frames and root
 root = Tk()
 titleFrame = Frame(root)
 underTitleFrame = Frame(root)
@@ -7,12 +8,32 @@ announcementBodyFrame = Frame(root)
 infoFrame = Frame(root)
 buttonFrame = Frame(root)
 
+#vars
+screenWidth = root.winfo_reqwidth()
+screenHeight = root.winfo_reqheight()
+buttonDimensionW = int(screenWidth / 9 + 1) #it isnt exactly right on the print button :/
+
+#sub frames
 submittedByFrame = Frame(infoFrame)
 infoFrameTitleFrame = Frame(infoFrame)
+subDateFrame = Frame(infoFrame)
+startAirFrame = Frame(infoFrame)
+endAirFrame = Frame(infoFrame)
+displayDaysFrame = Frame(infoFrame)
+commentsFrame = Frame(infoFrame)
+backFrame = Frame(buttonFrame)
+nextFrame = Frame(buttonFrame)
+saveFrame = Frame(buttonFrame)
+deleteFrame = Frame(buttonFrame)
+copyFrame = Frame(buttonFrame)
+printFrame = Frame(buttonFrame)
 
+#window setting
 root.title("Autoscript 3")
 root.geometry("1000x800")
+root.resizable(False, False)
 
+#functions for buttons
 def save():
     return "saved"
 
@@ -31,41 +52,50 @@ def copy():
 def print():
     return "printing"
 
-mainTitle = Label(titleFrame, text = "Autoscript 3\n", font=("Helvetica 45 bold"))
-#viewlast = Button(root, text = "Back", command = last())
-#viewNext = Button(root, text = "Next", command = next())
-#saveButton = Button(root, text = "Save", command = save())
-#deleteButton = Button(root, text = "Delete", command = delete())
-#copyButton = Button(root, text = "Copy", command = copy())
-#printButton = Button(root, text = "Print", command = print())
-announcementTitle = Label(underTitleFrame, text = "Add variable here")
-position = Label(underTitleFrame, text = "[" + "Add variable here" + "/" + "Add variable here" + "]")
-submitter = Label(submittedByFrame, text = "Add variable here")
-submitterFirst = Label(underTitleFrame, text = "(" + "Add variable here" + ")")
-#submissionDate = Label(root, text = "Add variable here")
-#startAir = Label(root, text = "Add variable here")
-#endAir = Label(root, text = "Add variable here")
-#displayDays = Label(root, text = "Add variable here")
-#submissionDateText = Label(root, text = "Submission Date:", font='Helvetica 18 bold')
-#startAirText = Label(root, text = "Add variable here", font='Helvetica 18 bold')
-#endAirText = Label(root, text = "Add variable here", font='Helvetica 18 bold')
-#displayDaysText = Label(root, text = "Add variable here", font='Helvetica 18 bold')
-submitterText = Label(submittedByFrame, text = "Submitted By", font='Helvetica 18 bold')
-infoText = Label(infoFrameTitleFrame, text = "Announcement Info", font='Helvetica 22 bold')
+#declare all labels, buttons, etc then pack them
+mainTitle = Label(titleFrame, text = "Autoscript 3\n", font=("Helvetica 45 bold")).pack(side=TOP)
+viewlast = Button(backFrame, text = "Back", command = last(), width = buttonDimensionW).pack()
+viewNext = Button(nextFrame, text = "Next", command = next(), width = buttonDimensionW).pack()
+saveButton = Button(saveFrame, text = "Save", command = save(), width = buttonDimensionW).pack()
+deleteButton = Button(deleteFrame, text = "Delete", command = delete(), width = buttonDimensionW).pack()
+copyButton = Button(copyFrame, text = "Copy", command = copy(), width = buttonDimensionW).pack()
+printButton = Button(printFrame, text = "Print", command = print(), width = buttonDimensionW).pack()
+announcementTitle = Label(underTitleFrame, text = "Add variable here").pack(side=LEFT)
+position = Label(underTitleFrame, text = "[" + "Add variable here" + "/" + "Add variable here" + "]").pack(side=LEFT)
+submitter = Label(submittedByFrame, text = "Add variable here").pack(side=BOTTOM)
+submitterFirst = Label(underTitleFrame, text = "(" + "Add variable here" + ")").pack(side=RIGHT)
+submissionDate = Label(subDateFrame, text = "Add variable here").pack(side=BOTTOM)
+startAir = Label(startAirFrame, text = "Add variable here").pack(side=BOTTOM)
+endAir = Label(endAirFrame, text = "Add variable here").pack(side=BOTTOM)
+displayDays = Label(displayDaysFrame, text = "Add variable here").pack(side=BOTTOM)
+submissionDateText = Label(subDateFrame, text = "Submission Date:", font='Helvetica 18 bold').pack(side=TOP)
+startAirText = Label(startAirFrame, text = "Start Date:", font='Helvetica 18 bold').pack(side=TOP)
+endAirText = Label(endAirFrame, text = "End Date:", font='Helvetica 18 bold').pack(side=TOP)
+displayDaysText = Label(displayDaysFrame, text = "Days Displayed:", font='Helvetica 18 bold').pack(side=TOP)
+submitterText = Label(submittedByFrame, text = "Submitted By:", font='Helvetica 18 bold').pack(side=TOP)
+infoText = Label(infoFrameTitleFrame, text = "Announcement Info", font='Helvetica 22 bold').pack(side=TOP)
+comments = Label(commentsFrame, text="Add Variable Here").pack(side=BOTTOM)
+commentsText = Label(commentsFrame, text = "Comments:", font='Helvetica 18 bold').pack(side=TOP)
 
-mainTitle.pack(side=TOP)
-position.pack(side=LEFT)
-announcementTitle.pack(side=LEFT)
-submitterFirst.pack(side=RIGHT)
-submitter.pack(side=BOTTOM)
-submitterText.pack(side=TOP)
-infoText.pack(side=TOP)
+#pack all the sub frames
+infoFrameTitleFrame.pack()
+submittedByFrame.pack()
+subDateFrame.pack()
+startAirFrame.pack()
+endAirFrame.pack()
+displayDaysFrame.pack()
+commentsFrame.pack()
+backFrame.pack(side=LEFT)
+nextFrame.pack(side=LEFT)
+saveFrame.pack(side=LEFT)
+deleteFrame.pack(side=LEFT)
+copyFrame.pack(side=LEFT)
+printFrame.pack(side=LEFT)
 
-infoFrameTitleFrame.pack(side=TOP)
-submittedByFrame.pack(side=TOP)
-
+#pack the main frames
 titleFrame.pack()
 underTitleFrame.pack()
-infoFrame.pack(side=LEFT)
+buttonFrame.pack(side=BOTTOM, anchor="w")
+infoFrame.pack(side=LEFT, anchor="n")
 
 root.mainloop()
--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-from tarfile import PAX_FIELDS
 from tkinter import *
+
+versionNum = "Version 1.0 "
 
 root = Tk()
 
+#functions
 def done():
-    return done
+    root.destroy()
 
+#window settings
 root.title("Autoscript 3")
 root.geometry("800x400")
+root.resizable(False, False)
 
+#set all frames
 titleFrame = Frame(root)
 anchorsFrame = Frame(root)
 anchorsFrame1 = Frame(anchorsFrame)
 anchorsFrame2 = Frame(anchorsFrame)
 doneFrame = Frame(root)
+verFrame = Frame(root)
 
+#declare all labels, buttons, etc
 blank = Label(doneFrame, text = "   ")
 mainTitle = Label(titleFrame, text = "Autoscript 3\n", font="Helvetica 45 bold")
 anchor1 = Label(anchorsFrame1, text = "Anchor 1:  ", font="Helvetica 15")
 anchor2 = Label(anchorsFrame2, text = "Anchor 2:  ", font="Helvetica 15")
 doneButton = Button(doneFrame, text = "Done", font="Helvetica 15", command = done(), activebackground = "black")
-sheetLink = Label(root, text = "Google Sheets File")
+#sheetLink = Label(, text = "Google Sheets File")
 inputAnchor1 = Entry(anchorsFrame1)
 inputAnchor2 = Entry(anchorsFrame2)
-inputSheetLink = Entry(root)
+#inputSheetLink = Entry()
+verNum = Label(verFrame, text = str(versionNum))
 
+#pack everything
 anchorsFrame1.pack(side=TOP)
 anchorsFrame2.pack(side=BOTTOM)
 mainTitle.pack(side=TOP)
@@ -36,9 +45,10 @@ anchor2.pack(side=LEFT)
 inputAnchor2.pack(side=LEFT)
 doneButton.pack(side=TOP)
 blank.pack(side=BOTTOM)
-
 titleFrame.pack()
-anchorsFrame.pack(padx=10)
+anchorsFrame.pack()
+verFrame.pack(side=BOTTOM, anchor="e")
 doneFrame.pack(side=BOTTOM)
+verNum.pack()
 
 root.mainloop()
