79ad81ec02d425bb6587bc7e3333795732720314
dylanrattai <817682@mystma.org>
2022-10-31 14:27:36 -0500

    closes window and opens index | optimization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from subprocess import call
 
 versionNum = "Version 1.0 "
 
@@ -7,7 +8,8 @@ root = Tk()
 #functions
 def done():
     root.destroy()
-
+    call(["python", "index.py"])
+    
 #window settings
 root.title("Autoscript 3")
 root.geometry("800x400")
@@ -22,33 +24,23 @@ doneFrame = Frame(root)
 verFrame = Frame(root)
 
 #declare all labels, buttons, etc
-blank = Label(doneFrame, text = "   ")
-mainTitle = Label(titleFrame, text = "Autoscript 3\n", font="Helvetica 45 bold")
-anchor1 = Label(anchorsFrame1, text = "Anchor 1:  ", font="Helvetica 15")
-anchor2 = Label(anchorsFrame2, text = "Anchor 2:  ", font="Helvetica 15")
-doneButton = Button(doneFrame, text = "Done", font="Helvetica 15", command = done(), activebackground = "black")
+blank = Label(doneFrame, text = "   ").pack(side=BOTTOM)
+mainTitle = Label(titleFrame, text = "Autoscript 3\n", font="Helvetica 45 bold").pack(side=TOP)
+anchor1 = Label(anchorsFrame1, text = "Anchor 1:  ", font="Helvetica 15").pack(side=LEFT)
+anchor2 = Label(anchorsFrame2, text = "Anchor 2:  ", font="Helvetica 15").pack(side=LEFT)
+doneButton = Button(doneFrame, text = "Done", font="Helvetica 15", command = done).pack(side=TOP)
 #sheetLink = Label(, text = "Google Sheets File")
-inputAnchor1 = Entry(anchorsFrame1)
-inputAnchor2 = Entry(anchorsFrame2)
+inputAnchor1 = Entry(anchorsFrame1).pack(side=LEFT)
+inputAnchor2 = Entry(anchorsFrame2).pack(side=LEFT)
 #inputSheetLink = Entry()
-verNum = Label(verFrame, text = str(versionNum))
+verNum = Label(verFrame, text = str(versionNum)).pack()
 
-#pack everything
+#pack frames
 anchorsFrame1.pack(side=TOP)
 anchorsFrame2.pack(side=BOTTOM)
-mainTitle.pack(side=TOP)
-#sheetLink.pack(row=2, column=0)
-#inputSheetLink.pack(row=2, column=1)
-anchor1.pack(side=LEFT)
-inputAnchor1.pack(side=LEFT)
-anchor2.pack(side=LEFT)
-inputAnchor2.pack(side=LEFT)
-doneButton.pack(side=TOP)
-blank.pack(side=BOTTOM)
 titleFrame.pack()
 anchorsFrame.pack()
 verFrame.pack(side=BOTTOM, anchor="e")
 doneFrame.pack(side=BOTTOM)
-verNum.pack()
 
 root.mainloop()
