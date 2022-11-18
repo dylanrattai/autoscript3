b25d7d93aecfc1bb78c9b20c200f181dd395b48f
dylanrattai <817682@mystma.org>
2022-11-18 14:13:42 -0600

    downloads now throught wget

--- background.py
+++ background.py
@@ -1,5 +1,6 @@
 from urllib import request
 from datetime import *
+import wget #needs pip install
 
 currentDateP = datetime.now()
 currentDate = currentDateP.strftime("%m-%d-%Y")
@@ -11,4 +12,5 @@ def exportSheet(): #this method of downloading will eventually become deprecated
     print(anchor1)
     print(anchor2)
     localFile = "./sheets/" + currentDate + "test.xlsx"
-    request.urlretrieve("https://docs.google.com/spreadsheets/d/1QliSLTkS2qLmRr64Gs0RKU-fxZ_zyoJSf3fb1uxA7nA/export?format=xlsx", localFile)
+    #request.urlretrieve("https://docs.google.com/spreadsheets/d/1QliSLTkS2qLmRr64Gs0RKU-fxZ_zyoJSf3fb1uxA7nA/export?format=xlsx", localFile)
+    wget.download("https://docs.google.com/spreadsheets/d/1QliSLTkS2qLmRr64Gs0RKU-fxZ_zyoJSf3fb1uxA7nA/export?format=xlsx", localFile)
